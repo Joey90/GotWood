08c6acd697461a038435964ab9643ec8f01ee680
William Woodhead <william.woodhead@lux01.co.uk>
2013-07-15 20:28:21 +0100

    Added ports to the canvas map.

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -3,6 +3,7 @@ var canvas = document.getElementById('mapCanvas');
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
         drawMap(ctx, TestingData.tileData);
+        drawPorts(ctx, TestingData.portData);
     }
 }
 
@@ -71,4 +72,138 @@ function drawHexagon(ctx, centreX, centreY, length, fillStyle, strokeStyle, line
     ctx.fillStyle = fillStyle;
     ctx.stroke();
     ctx.fill();
+}
+
+function drawPorts(ctx, portData) {
+    // We need to be able to work out the "anchor" vertex of each port,
+    var spaceX  = Config.Graphics.length * Math.sqrt(3);
+    var offsetX = - Config.Graphics.length * Math.sqrt(3) / 2;
+    var offsetY = 3 * Config.Graphics.length / 2;
+    var cosPi6  = Math.cos(Math.PI/6);
+    var sinPi6  = 0.5;
+    
+    for(var i = 0; i < Config.Ports.locations.length; i++) {
+        
+        var xMod = 0;
+        var yMod = 0;
+        var angle = 0;
+        
+        switch(Config.Ports.anchor[i]) {
+            case 0:
+                xMod = 0;
+                yMod = - Config.Graphics.length;
+                angle = - Math.PI / 3;
+                break;
+            case 1:
+                xMod = Config.Graphics.length * cosPi6;
+                yMod = - Config.Graphics.length / 2;
+                angle = 0;
+                break;
+            case 2:
+                xMod = Config.Graphics.length * cosPi6;
+                yMod = Config.Graphics.length / 2;
+                angle = Math.PI / 3;
+                break;
+            case 3:
+                xMod = 0;
+                yMod = Config.Graphics.length;
+                angle = 2 * Math.PI / 3;
+                break;
+            case 4:
+                xMod = - Config.Graphics.length * cosPi6;
+                yMod = Config.Graphics.length / 2;
+                angle = Math.PI;
+                break;
+            case 5:
+                xMod = - Config.Graphics.length * cosPi6;
+                yMod = - Config.Graphics.length / 2;
+                angle = -2 * Math.PI / 3;
+                break;
+        }
+    
+        var xPos = Config.Graphics.startX
+                    + offsetX * Config.Graphics.xOffsets[ Config.Ports.locations[i] ]
+                    + spaceX  * Config.Graphics.spaces[ Config.Ports.locations[i] ]
+                    + xMod;
+        var yPos = Config.Graphics.startY
+                    + offsetY * Config.Graphics.yOffsets[ Config.Ports.locations[i] ]
+                    + yMod;
+                    
+        // Save the context state
+        ctx.save();
+        
+        // Transform the context so that the port edge is aligned with the y-axis
+        ctx.translate(xPos, yPos);
+        ctx.rotate(angle);
+        
+        // Draw the port trapezium
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(Config.Graphics.portLength * cosPi6, Config.Graphics.portLength * sinPi6);
+        ctx.lineTo(Config.Graphics.portLength * cosPi6,
+            Config.Graphics.length - Config.Graphics.portLength * sinPi6);
+        ctx.lineTo(0, Config.Graphics.length);
+        ctx.closePath();
+        
+        // Fill in dat trapezium
+        ctx.lineWidth = Config.Graphics.lineWidth;
+        ctx.strokeStyle = Config.Graphics.strokeStyle;
+        ctx.fillStyle = Config.Graphics.portFill;
+        ctx.stroke();
+        ctx.fill();
+        
+        // Save the context
+        ctx.save();
+        ctx.rotate(Math.PI/2);
+        
+        // Write the text
+        ctx.font = Config.Graphics.font;
+        ctx.fillStyle = Config.Graphics.fontColor;
+        ctx.textAlign = 'center';
+        
+        var tradeRate = '2:1';
+        var resource = '';
+        switch(portData[i].resource) {
+            case TileEnums.DESERT:
+                resource = 'ANY';
+                tradeRate = '3:1';
+                break;
+            case TileEnums.WOOD:
+                resource = 'WOOD';
+                break;
+            case TileEnums.BRICK:
+                resource = 'BRICK';
+                break;
+            case TileEnums.WHEAT:
+                resource = 'WHEAT';
+                break;
+            case TileEnums.WOOL:
+                resource = 'WOOL';
+                break;
+            case TileEnums.ORE:
+                resource = 'ORE';
+                break;
+        }
+
+        ctx.font = Config.Graphics.font;
+        ctx.fillStyle = Config.Graphics.fontColor;
+        ctx.textAlign = 'center';
+        
+        // If the anchor is 2 or 3, we rotate the context once more
+        // and rotate to make the text render the right way up
+        if( Config.Ports.anchor[i] == 2 || Config.Ports.anchor[i] == 3 ) {
+            ctx.save();
+            ctx.rotate(Math.PI);
+            
+            ctx.fillText(tradeRate, -Config.Graphics.length/2,20);
+            ctx.fillText(resource, -Config.Graphics.length/2, 50);
+            ctx.restore();
+        } else {
+            ctx.fillText(tradeRate, Config.Graphics.length/2, -5);
+            ctx.fillText(resource, Config.Graphics.length/2, -30);
+        }
+        // Fully restore the context
+        ctx.restore();
+        ctx.restore();
+    }
 }
--- canvas/config.js
+++ canvas/config.js
@@ -16,14 +16,21 @@ Config = {
         startX  : 313,
         startY  : 155,
         length  : 80,
+        portLength: 30,
         tiles   : [],
         oceanFill  : 'RoyalBlue',
-        strokeStyle: 'rgb(0,0,0)',
+        portFill   : 'Gold',
+        strokeStyle: 'Black',
         lineWidth  : 1,
         font       : '20px Denk One',
         fontColor  : 'Black',
         textAlign  : 'center'
-    }
+    },
+    Ports: {
+        locations : [0, 1, 3, 6, 11, 12, 15, 16, 17],
+        anchor    : [5, 0, 4, 0, 1, 4, 2, 3, 2]
+    }, 
+    Resources: []
 }
 
 Config.Graphics.tiles[TileEnums.DESERT] = { fill: 'LightGoldenrodYellow', label: 'DESERT' };
@@ -33,6 +40,11 @@ Config.Graphics.tiles[TileEnums.WHEAT]  = { fill: 'Wheat', label: 'FIELD' };
 Config.Graphics.tiles[TileEnums.WOOL]   = { fill: 'PaleGreen', label: 'PASTURE' };
 Config.Graphics.tiles[TileEnums.ORE]    = { fill: 'Silver', label: 'MOUNTAIN' };
 
+Config.Resources[TileEnums.WOOD]  = { name: 'Wood' };
+Config.Resources[TileEnums.BRICK] = { name: 'Brick' };
+Config.Resources[TileEnums.WHEAT] = { name: 'Wheat' };
+Config.Resources[TileEnums.WOOL]  = { name: 'Wool' };
+Config.Resources[TileEnums.ORE]   = { name: 'Ore' };
 
 TestingData = {
     tileData : [
@@ -55,5 +67,16 @@ TestingData = {
         { tile: TileEnums.ORE, number: 5 },
         { tile: TileEnums.WHEAT, number: 2 },
         { tile: TileEnums.WOOD, number: 6 }
+    ],
+    portData : [
+        { resource: TileEnums.DESERT },
+        { resource: TileEnums.WOOL },
+        { resource: TileEnums.ORE },
+        { resource: TileEnums.DESERT },
+        { resource: TileEnums.DESERT },
+        { resource: TileEnums.WHEAT },
+        { resource: TileEnums.BRICK },
+        { resource: TileEnums.DESERT },
+        { resource: TileEnums.WOOD }
     ]
 }
